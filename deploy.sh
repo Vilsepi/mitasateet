3ddfac509b85e5c68012ac8b58a7c678ecbce2f3
Ville Seppanen <ville.seppanen@gofore.com>
2016-09-29 22:06:54 +0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script syncs the current directory to S3 to be hosted as a static website
+
+# Bash strict mode
+set -euo pipefail
+
+REGION="eu-west-1"
+PROFILE="heap"
+BUCKET="mitasateet.heap.fi"
+
+SYNC_BASE_ARGS="s3://$BUCKET/ --profile $PROFILE --region $REGION"
+SYNC_FILE_ARGS="--exclude '*' --include '*.html' --include 'assets/*'"
+
+if [ $# -lt 1 ]
+then
+  echo "Simulating S3 sync, no changes are made"
+  eval aws s3 sync . $SYNC_BASE_ARGS $SYNC_FILE_ARGS --dryrun
+  echo "To deploy, re-execute with --doit"
+else
+  if [ $1 == "--doit" ]; then
+    echo "Uploading assets"
+    eval aws s3 sync . $SYNC_BASE_ARGS $SYNC_FILE_ARGS
+  else
+    echo "Unknown arguments. Either use no args to dryrun or use --doit to upload"
+  fi
+fi
