a653230e7de08b8c32cf05a5c5b3fb47bf7b2650
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2016-09-30 16:23:22 +0300

    feat: simple linear font scaling based on input length

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -9,7 +8,29 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+var MAX_FONT_SIZE = 21;
+var MIN_FONT_SIZE = 13;
+var CUTOFF = 16;
+var MAX_LENGTH = 64;
+
+function scaleFontSize(len) {
+  if (len < CUTOFF) {
+    return MAX_FONT_SIZE;
+  }
+  return MAX_FONT_SIZE - (MAX_FONT_SIZE - MIN_FONT_SIZE) * ((len - CUTOFF) / (MAX_LENGTH - CUTOFF));
+}
+
+function prepareText(text) {
+  if (text.length > MAX_LENGTH) {
+    text = text.substring(0, MAX_LENGTH);
+  }
+  return {size: scaleFontSize(text.length), comment: text};
+}
+
 var comment = getParameterByName('q');
-if(comment) {
-    document.getElementById("comment").appendChild(document.createTextNode(comment));
+if (comment) {
+    var el = document.getElementById("comment");
+    var text = prepareText(comment);
+    el.appendChild(document.createTextNode(text.comment));
+    el.style['font-size'] = text.size + 'px';
 }
